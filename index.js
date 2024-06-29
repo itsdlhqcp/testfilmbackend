3753e679a7b8b635b609f4845b07bcb85ce681b1
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-06-29 21:03:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,18 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({ origin: "https://test-filmb.vercel.app", credentials: true }));
+// app.use(cors({ origin: "https://test-filmb.vercel.app", credentials: true }));
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (origin === 'https://test-filmb.vercel.app') {
+      return callback(null, true);
+    }
+    callback(new Error('Not allowed by CORS'));
+  },
+  credentials: true
+}));
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
